02be40f37f4601093e601c3d76e3958433529703
mattiaS <mattia@3doers.it>
2016-11-18 17:12:39 +0000

    add task tab and task creator

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('ng-admin');
 const messageEntity = require('./entity/messages');
 const contactEntity = require('./entity/contacts');
 const appointmentEntity = require('./entity/appointments');
+const tasksEntity = require('./entity/tasks');
 const headerTemplate = require('./header/header.js');
 
 const app = angular.module('roundAbout', ['ng-admin'])
@@ -11,6 +12,7 @@ const app = angular.module('roundAbout', ['ng-admin'])
     const message = messageEntity(nga, admin);
     const contact = contactEntity(nga, admin);
     const appointment = appointmentEntity(nga, admin);
+    const tasks = tasksEntity(nga, admin);
 
     const header = headerTemplate(nga, admin);
 
@@ -20,6 +22,7 @@ const app = angular.module('roundAbout', ['ng-admin'])
         .addChild(nga.menu(contact).icon('<span class="glyphicon glyphicon-user"></span>'))
         .addChild(nga.menu(message).icon('<span class="glyphicon glyphicon-envelope"></span>'))
         .addChild(nga.menu(appointment).icon('<span class="glyphicon glyphicon-calendar"></span>'))
+        .addChild(nga.menu(tasks).icon('<span class="glyphicon glyphicon-calendar"></span>'))
     );
   }]);
 
