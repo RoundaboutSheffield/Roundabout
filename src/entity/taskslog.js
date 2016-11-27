bf625c8a164464b1bda81f5da552c02b8e83d93d
mattiaS <mattia@3doers.it>
2016-11-27 22:32:02 +0000

    add config.env to .gitignore

--- src/entity/taskslog.js
+++ src/entity/taskslog.js
@@ -5,17 +5,15 @@ module.exports = (nga, admin) => {
     .fields([
       nga.field('taskName'),
       nga.field('tenantReplyReceived'),
-      nga.field('completionValidatedByAdmin')
+      nga.field('completionValidatedByAdmin'),
     ]);
 
+  admin.addEntity(task);
 
-
-    admin.addEntity(task);
-
-    dpd.on('apiError', () => {
+  dpd.on('apiError', () => {
       // eslint-disable-next-line no-alert
-      alert('Error: Nexmo key missing. See project readme for correct way to execute application');
-    });
+    alert('Error: Nexmo key missing. See project readme for correct way to execute application');
+  });
 
-    return task;
+  return task;
 };
