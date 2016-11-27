808523fd40e1b6e4dd5d5a41d981b45028fedae5
mattiaS <mattia@3doers.it>
2016-11-27 23:06:32 +0000

    add filter function in tasklog

--- src/entity/taskslog.js
+++ src/entity/taskslog.js
@@ -1,6 +1,15 @@
 module.exports = (nga, admin) => {
   const task = nga.entity('taskslog');
 
+  const fields = [
+    nga.field('tasksName'),
+    nga.field('tenantName'),
+    nga.field('dateAssigned'),
+    nga.field('tenantReplyReceived'),
+    nga.field('completionValidateByAdmin'),
+  ];
+
+
   task.listView()
     .fields([
       nga.field('taskName'),
@@ -9,12 +18,13 @@ module.exports = (nga, admin) => {
       .format('dd-MM-yyyy HH:mm:ss'),
       nga.field('tenantReplyReceived'),
       nga.field('completionValidatedByAdmin'),
-    ]);
+    ])
+    .filters(fields);
 
   admin.addEntity(task);
 
   dpd.on('apiError', () => {
-      // eslint-disable-next-line no-alert
+    // eslint-disable-next-line no-alert
     alert('Error: Nexmo key missing. See project readme for correct way to execute application');
   });
 
