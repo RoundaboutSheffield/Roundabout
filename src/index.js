60df9a45915291f072650b6d7712b6cd59179883
Matt Glover <matthewglover@me.com>
2016-11-29 16:48:27 +0000

    feat(taskslog): update taskslog to exclude date from edit form
    avoids issues with date conversion errors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ require('ng-admin');  // NOTE: ng-admin needs to be a dev dependency
 const userEntity = require('./entity/users');
 const messageEntity = require('./entity/messages');
 const contactEntity = require('./entity/contacts');
-const appointmentEntity = require('./entity/appointments');
+// const appointmentEntity = require('./entity/appointments');
 const tasksEntity = require('./entity/tasks');
 const logsEntity = require('./entity/taskslog');
 const headerTemplate = require('./header/header.js');
@@ -17,7 +17,7 @@ const app = angular.module('roundAbout', ['ng-admin'])
     const user = userEntity(nga, admin);
     const message = messageEntity(nga, admin);
     const contact = contactEntity(nga, admin);
-    const appointment = appointmentEntity(nga, admin);
+    // const appointment = appointmentEntity(nga, admin);
     const tasks = tasksEntity(nga, admin);
     const logger = logsEntity(nga, admin);
 
@@ -30,11 +30,10 @@ const app = angular.module('roundAbout', ['ng-admin'])
       .addChild(nga.menu(user).icon('<span class="glyphicon glyphicon-user"></span>'))
       .addChild(nga.menu(contact).icon('<span class="glyphicon glyphicon-user"></span>'))
       .addChild(nga.menu(message).icon('<span class="glyphicon glyphicon-envelope"></span>'))
-      .addChild(nga.menu(appointment).icon('<span class="glyphicon glyphicon-calendar"></span>'))
+      // .addChild(nga.menu(appointment).icon('<span class="glyphicon glyphicon-calendar"></span>'))
       .addChild(nga.menu(tasks).icon('<span class="glyphicon glyphicon-calendar"></span>'))
       .addChild(nga.menu(logger).icon('<span class="glyphicon glyphicon-calendar"></span>')));
   }]);
 
 require('./config/restangularConfig')(app);
 require('./directives/logoutBtn')(app);
-
