b4c7547ca97331d4e7a1909fc557e16288f8d646
Matt Glover <matthewglover@me.com>
2016-12-01 12:04:04 +0000

    feat(message): set message to task details

--- resources/messages/post.js
+++ resources/messages/post.js
@@ -1,4 +1,11 @@
-const sendMessage = require('../../lib/send_message');
+const env = process().env;
+
+if (!env.NEXMO_KEY || !env.NEXMO_SECRET || !env.NEXMO_VIRTUAL_NUMBER) {
+  emit('apiError');
+  cancel('Missing API key');
+}
+
+const sendMessage = require('../../lib/send_message')(env);
 const prop = require('../../lib/prop');
 const trace = require('../../lib/trace');
 
@@ -10,13 +17,18 @@ const { to, message, taskId } = this;
 
 // Get contact by message.to
 const contactPromise = dpd.contacts.get({id: to});
+const taskPromise = dpd.tasks.get({id: taskId});
 
+// Declare some helper functions
 const getUserIdFromContact = () => contactPromise.then(prop('userId'));
 const updateTasksLog = tenantId => dpd.taskslog.post({ taskId, dateAssigned: Date.now(), tenantId });
+const getDetails = ([{phoneNumber}, {details}]) => [phoneNumber, details];
 
-contactPromise
-  .then(prop('phoneNumber'))
-  .then(sendMessage(message))
-  .then(getUserIdFromContact)
-  .then(updateTasksLog)
-  .catch(trace('Message error:'));
+// Send message and then update taskslog
+Promise.all([contactPromise, taskPromise])
+    .then(getDetails)
+    .then(sendMessage)
+    .then(getUserIdFromContact)
+    .then(updateTasksLog)
+    .catch(trace('Message error:'));
+
