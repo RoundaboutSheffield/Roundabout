d31371ba3d8f284f619e6c17029bdc33ee9f9172
Matt Glover <matthewglover@me.com>
2016-12-01 16:07:14 +0000

    refactor(config): refactor RestangularProvider config to functions

--- src/config/restangularConfig.js
+++ src/config/restangularConfig.js
@@ -1,47 +1,53 @@
+const { prop, pick, mergeAll, compose, forEach, keys, evolve } = require('ramda');
+
+const dir = direction =>
+  (direction === 'DESC'
+    ? -1
+    : 1);
+
+const $skip = ({ _page, _perPage }) =>
+  ({ $skip: (_page - 1) * _perPage });
+
+const $limit =
+  pick(['_perPage']);
+
+const sort = ({ _sortField, _sortDir }) =>
+  (_sortField
+    ? { [_sortField]: dir(_sortDir) }
+    : {});
+
+const $sort = params =>
+  ({ $sort: sort(params) });
+
+const toRegExFilter = filter =>
+  ({
+    $regex: filter,
+    $options: 'i',
+  });
+
+const setRegExFilters =
+  evolve({
+    username: toRegExFilter,
+  });
+
+const filters = compose(setRegExFilters, prop('_filters'));
+
+const cleanseParams = (obj) => {
+  forEach(k => delete obj[k], keys(obj));
+  return obj;
+};
+
 module.exports = app =>
   app
   .config(['RestangularProvider', (RestangularProvider) => {
     RestangularProvider.addFullRequestInterceptor(
       (element, operation, what, url, headers, params) => {
-        if (operation === 'getList') {
-          const dir = params._sortDir === 'DESC' ? -1 : 1;
-          const sort = {};
-
-          params.$skip = (params._page - 1) * params._perPage;
-          params.$limit = params._perPage;
-
-          if (params._sortField) {
-            sort[params._sortField] = dir;
-            params.$sort = sort;
-          }
-
-          delete params._page;
-          delete params._perPage;
-          delete params._sortField;
-          delete params._sortDir;
-        }
-
-        if (operation === 'getList' && params._filters) {
-          Object.keys(params._filters).reduce((acc, filter) => {
-            // NOTE: this is a custom solution - perhaps refactor?
-            if (filter === 'id' || filter === 'isAdmin') {
-              const idFilter = params._filters[filter];
-              return Object.assign(acc, { [filter]: idFilter });
-            }
-
-            // NOTE: Where is this code used? It's overriding all options (e.g. isAdmin = true)
-            const regexFilter = {
-              $regex: params._filters[filter],
-              $options: 'i',
-            };
-
-            Object.assign(acc, { [filter]: regexFilter });
-
-            return acc;
-          }, params);
-
-          delete params._filters;
-        }
-        return { params };
+        const newParams = mergeAll([
+          filters(params),
+          $sort(params),
+          $skip(params),
+          $limit(params),
+        ]);
+        Object.assign(cleanseParams(params), newParams);
       });
   }]);
